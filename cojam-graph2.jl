68f7f3d367809d79138b1a5453becd63f83c1499
Uwe L. Korn <uwelk@xhochy.com>
2014-02-03 14:55:26 +0000

    Add progress for cojam-graph2

--- cojam-graph2.jl
+++ cojam-graph2.jl
@@ -18,8 +18,17 @@ let artists_u = Dict{UTF8String, Vector{UTF8String}}()
         end
     end
 
+    len = length(artists_u)
+    i = 0
     for artist in artists_u
+        @printf("\rProcessing artist crossproduct % 6d/%d (% 3d%%)                    ", i, len, div(i,len))
+        i += 1
+        j = 0
         for user1 in artist[2]
+            j += 1
+            if j % 25 == 0
+                @printf("\rProcessing artist crossproduct % 6d/%d (% 3d%%) (user % 4d/%d)", i, len, div(i,len), j, length(artist[2]))
+            end
             for user2 in artist[2]
                 if user1 < user2
                     if !haskey(users, (user1, user2))
