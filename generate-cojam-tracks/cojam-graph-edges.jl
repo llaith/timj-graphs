271be80e975e7a7b3ba6fc166ba9e13ef93c121c
Uwe L. Korn <uwelk@xhochy.com>
2014-02-17 16:57:17 +0100

    Partition inside Julia code

--- generate-cojam-tracks/cojam-graph-edges.jl
+++ generate-cojam-tracks/cojam-graph-edges.jl
@@ -68,19 +68,24 @@ function write_edges(users::Dict{UTF8String, Dict{UTF8String,Int}})
 end
 
 min_intersection = int(ARGS[1])
-lower_idx = int(ARGS[2])
-upper_idx = int(ARGS[3])
 users = Dict{UTF8String, Dict{UTF8String,Int}}()
 
 (tracks_u, user_id2idx) = parsecsv()
 graph_file = open(string("cojam-tracks-",min_intersection,".graphml"), "a")
 
-@printf("Generating edges (u,v) for %d <= u < %d…", lower_idx, upper_idx)
-users = generate_edges(tracks_u, user_id2idx, lower_idx, upper_idx)
-println("done.")
+partition = [0       160000 ; 160000  320000; 320000  480000; 480000  640000; 640000  720000; 720000  800000; 800000  880000; 880000  960000; 960000  1040000; 1040000 1120000; 1120000 1200000; 1200000 1280000; 1280000 1360000; 1360000 1440000; 1440000 1480000; 1480000 1500000; 1500000 1520000; 1520000 1530000; 1530000 1540000; 1540000 1550000; 1550000 1555000; 1555000 1560000; 1560000 1565000; 1565000 1570000; 1570000 1571000; 1571000 1572000; 1572000 1573000; 1573000 1574000; 1574000 1575000]
 
+for i in 1:size(partition,1)
+    lower_idx = partition[i,1]
+    upper_idx = partition[i,2]
+    @printf("Generating edges (u,v) for %d <= u < %d…", lower_idx, upper_idx)
+    users = generate_edges(tracks_u, user_id2idx, lower_idx, upper_idx)
+    println("done.")
 
-print("Writing edges…")
-num_edges = write_edges(users)
-@printf("wrote %d edges.\n", num_edges)
+    print("Writing edges…")
+    num_edges = write_edges(users)
+    @printf("wrote %d edges.\n", num_edges)
+end
+
 close(graph_file)
+
--- generate-cojam-tracks/cojam-graph-runner.sh
+++ generate-cojam-tracks/cojam-graph-runner.sh
@@ -4,35 +4,7 @@ echo "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:xsi=\"http:
 echo "<graph id=\"G\" edgedefault=\"undirected\">" >> cojam-tracks-$1.graphml
 echo "  <key id=\"cojams_num\" for=\"edge\" attr.name=\"cojams_num\" attr.type=\"int\"/>" >> cojam-tracks-$1.graphml
 julia cojam-graph-vertices.jl $1
-julia cojam-graph-edges.jl $1 0       160000
-julia cojam-graph-edges.jl $1 160000  320000
-julia cojam-graph-edges.jl $1 320000  480000
-julia cojam-graph-edges.jl $1 480000  640000
-julia cojam-graph-edges.jl $1 640000  720000
-julia cojam-graph-edges.jl $1 720000  800000
-julia cojam-graph-edges.jl $1 800000  880000
-julia cojam-graph-edges.jl $1 880000  960000
-julia cojam-graph-edges.jl $1 960000  1040000
-julia cojam-graph-edges.jl $1 1040000 1120000
-julia cojam-graph-edges.jl $1 1120000 1200000
-julia cojam-graph-edges.jl $1 1200000 1280000
-julia cojam-graph-edges.jl $1 1280000 1360000
-julia cojam-graph-edges.jl $1 1360000 1440000
-julia cojam-graph-edges.jl $1 1440000 1480000
-julia cojam-graph-edges.jl $1 1480000 1500000
-julia cojam-graph-edges.jl $1 1500000 1520000
-julia cojam-graph-edges.jl $1 1520000 1530000
-julia cojam-graph-edges.jl $1 1530000 1540000
-julia cojam-graph-edges.jl $1 1540000 1550000
-julia cojam-graph-edges.jl $1 1550000 1555000
-julia cojam-graph-edges.jl $1 1555000 1560000
-julia cojam-graph-edges.jl $1 1560000 1565000
-julia cojam-graph-edges.jl $1 1565000 1570000
-julia cojam-graph-edges.jl $1 1570000 1571000
-julia cojam-graph-edges.jl $1 1571000 1572000
-julia cojam-graph-edges.jl $1 1572000 1573000
-julia cojam-graph-edges.jl $1 1573000 1574000
-julia cojam-graph-edges.jl $1 1574000 1575000
+julia cojam-graph-edges.jl $1
 echo "</graph>" >> cojam-tracks-$1.graphml
 echo "</graphml>" >> cojam-tracks-$1.graphml
 EDGES=`grep '</edge>' cojam-tracks-$1.graphml | wc -l`
