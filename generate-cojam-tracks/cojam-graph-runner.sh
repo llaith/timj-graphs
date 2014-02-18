bba5f821fa010c9b4961aac0fa39b19ce07ce5f7
Uwe L. Korn <uwelk@xhochy.com>
2014-02-18 01:02:06 +0100

    Renamed julia cojam-tracks script

--- generate-cojam-tracks/cojam-graph-runner.sh
+++ generate-cojam-tracks/cojam-graph-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-julia cojam-graph-edges.jl $1
+julia cojam-tracks-graph.jl $1
 EDGES=`grep '</edge>' cojam-tracks-$1.graphml | wc -l`
 VERTICES=`grep '<node ' cojam-tracks-$1.graphml | wc -l`
 echo Generated graph with $VERTICES vertices and $EDGES edges.
