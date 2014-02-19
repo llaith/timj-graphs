0b64935aae0ab2e7db455dca21e664a3db4f9771
Uwe L. Korn <uwelk@xhochy.com>
2014-02-19 21:06:13 +0100

    Remove non-connected vertices in post-processing

--- generate-cojam-tracks/cojam-graph-runner.sh
+++ generate-cojam-tracks/cojam-graph-runner.sh
@@ -3,3 +3,7 @@ julia cojam-tracks-graph.jl $1
 EDGES=`grep '</edge>' cojam-tracks-$1.graphml | wc -l`
 VERTICES=`grep '<node ' cojam-tracks-$1.graphml | wc -l`
 echo Generated graph with $VERTICES vertices and $EDGES edges.
+julia ../strip-non-connected-vertices.jl cojam-tracks-$1.graphml
+EDGES=`grep '</edge>' cojam-tracks-$1.graphml | wc -l`
+VERTICES=`grep '<node ' cojam-tracks-$1.graphml | wc -l`
+echo Generated graph with $VERTICES vertices and $EDGES edges.
